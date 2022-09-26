b7b00febc638cec5d9cdaca9a6d7c268aceec1d2
fire-walk-with-me <mrc.e.bra@gmail.com>
2022-09-26 13:41:00 +0200

    fixed PlayerEnterCheck

--- Reflected/Assets/Scripts/AI-Director/PlayerEnterCheck.cs
+++ Reflected/Assets/Scripts/AI-Director/PlayerEnterCheck.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class PlayerEnterCheck : MonoBehaviour
 {
-    [SerializeField] Player player;
     [SerializeField] AiDirector aiDirector;
     void Start()
     {
-        if (!player) player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         if (!aiDirector) aiDirector = GameObject.FindGameObjectWithTag("GameManager").GetComponent<AiDirector>();
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerExit(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("Enterd");
+            //Debug.Log("Enterd");
             aiDirector.EnterRoom();
         }
     }
