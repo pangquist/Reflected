ef80a04a4c189477e41892156d877a7cf768515e
MarcusS64 <marcus@skolds.com>
2022-09-26 14:50:22 +0200

    Item database fix

--- Reflected/Assets/Scripts/Inventory/ItemDatabaseData.cs
+++ Reflected/Assets/Scripts/Inventory/ItemDatabaseData.cs
@@ -9,14 +9,14 @@ public class ItemDatabaseData : ScriptableObject
     public Dictionary<ItemData, int> GetId = new Dictionary<ItemData, int>();
     public Dictionary<int, ItemData> GetItem = new Dictionary<int, ItemData>();
 
-    public void OnAfterDeserialize()
-    {
-        GetId = new Dictionary<ItemData, int>();
-        GetItem = new Dictionary<int, ItemData>();
-        for (int i = 0; i < items.Length; i++)
-        {
-            GetId.Add(items[i], i);
-            GetItem.Add(i, items[i]);
-        }
-    }
+    //public void OnAfterDeserialize()
+    //{
+    //    GetId = new Dictionary<ItemData, int>();
+    //    GetItem = new Dictionary<int, ItemData>();
+    //    for (int i = 0; i < items.Length; i++)
+    //    {
+    //        GetId.Add(items[i], i);
+    //        GetItem.Add(i, items[i]);
+    //    }
+    //}
 }
