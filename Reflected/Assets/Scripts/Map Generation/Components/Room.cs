4706909c079ddc64fab62b2289bbd366705ebf54
fire-walk-with-me <mrc.e.bra@gmail.com>
2022-10-03 17:11:50 +0200

    added prefab for spawnpoint-prefab and check when player enter new room

--- Reflected/Assets/Scripts/Map Generation/Components/Room.cs
+++ Reflected/Assets/Scripts/Map Generation/Components/Room.cs
@@ -13,6 +13,8 @@ public class Room : MonoBehaviour
 
     private static Map map;
 
+    AiDirector aiDirector;
+
     // Properties
 
     public RectInt Rect => rect;
@@ -72,6 +74,15 @@ public class Room : MonoBehaviour
 
         foreach (Chamber chamber in chambers)
             chamber.gameObject.SetActive(true);
+
+        if (!aiDirector) aiDirector = GameObject.FindGameObjectWithTag("GameManager").GetComponent<AiDirector>();
+
+        if (!cleared)
+        {
+            if (!aiDirector) aiDirector = GameObject.FindGameObjectWithTag("GameManager").GetComponent<AiDirector>();
+
+            aiDirector.EnterRoom();
+        }
     }
 
     public void SetCleared(bool cleared)
