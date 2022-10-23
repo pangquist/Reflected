f9b37ce0793d6cb041b1cf3301e60926eedfe72a
Kevin Bjørn Roulund Wilken <kwilken1608@hotmail.dk>
2022-10-23 15:45:09 +0200

    Folder Cleanup
    Removed old scripts and prefabs.

--- Reflected/Assets/Scripts/Enemies/PlaceholderEnemyScript.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlaceholderEnemyScript : MonoBehaviour
-{
-    //Replace with the actual enemy script here.
-    //Currently only used to assign player as target in AiManager.
-}
