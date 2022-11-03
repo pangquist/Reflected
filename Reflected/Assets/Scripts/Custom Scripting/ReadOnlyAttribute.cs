cfc51ebf87aa3a99b84f32756b5a23edde217cce
FilipTrip <81421618+FilipTrip@users.noreply.github.com>
2022-11-03 15:04:40 +0100

    ReadOnlyAttribute potential fix

--- Reflected/Assets/Scripts/Custom Scripting/ReadOnlyAttribute.cs
+++ Reflected/Assets/Scripts/Custom Scripting/ReadOnlyAttribute.cs
@@ -1,25 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class ReadOnlyAttribute : PropertyAttribute
 {
-
+    // Is supposed to be empty
 }
 
-[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-public class ReadOnlyDrawer : PropertyDrawer
-{
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-    {
-        return EditorGUI.GetPropertyHeight(property, label, true);
-    }
-
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = true;
-    }
-}
