ed147197e11d32493f9725bbf4b01d46c8ea128e
Kevin Bjørn Roulund Wilken <kwilken1608@hotmail.dk>
2022-11-08 14:53:44 +0100

    Fixed math with attack rate

--- Reflected/Assets/Scripts/Enemy-AI/AoeAttackState.cs
+++ Reflected/Assets/Scripts/Enemy-AI/AoeAttackState.cs
@@ -27,7 +27,7 @@ public class AoeAttackState : State
     public override void DoState(AiManager2 thisEnemy, Player player, NavMeshAgent agent, EnemyStatSystem enemyStatSystem)
     {
         //Set relevant stats
-        attackRate = baseAttackRate * enemyStatSystem.GetAttackSpeed(); //Right now the attack speed increases but decreases the attack rate. High attack rate = low attack speed.
+        attackRate = baseAttackRate / enemyStatSystem.GetAttackSpeed();
         //Attack range?
 
         //If ranged and too close, move away from target.
