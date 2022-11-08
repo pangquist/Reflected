8b4b8ad6c899bfd0b5cf39bf875bcd076fd6dbb5
Kevin Bjørn Roulund Wilken <kwilken1608@hotmail.dk>
2022-11-08 14:35:45 +0100

    Renamed Prefabs and deleted old ones

--- Reflected/Assets/Scripts/AI-Director/EnemySpawner.cs
+++ Reflected/Assets/Scripts/AI-Director/EnemySpawner.cs
@@ -88,7 +88,7 @@ public class EnemySpawner : MonoBehaviour
 
         if (percentage <= spawnBias)
         {
-            enemyToSpawn = enemyDOT;
+            enemyToSpawn = enemyClose;
         }
         else
         {
