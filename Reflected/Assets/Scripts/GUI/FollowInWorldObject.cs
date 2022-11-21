efc1a2866fab3378d04b98e29bb122690ab7e943
FilipTrip <81421618+FilipTrip@users.noreply.github.com>
2022-11-21 16:57:43 +0100

    New health bars

---
+++ Reflected/Assets/Scripts/GUI/FollowInWorldObject.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowInWorldObject : MonoBehaviour
+{
+    [SerializeField] private Transform objectToFollow;
+    [SerializeField] private Vector3 offset;
+
+    private RectTransform rectTransform;
+
+    private static Canvas canvas;
+    private static Camera mainCamera;
+
+    private void Awake()
+    {
+        if (canvas == null)
+        {
+            canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+            mainCamera = Camera.main;
+        }
+
+        rectTransform = GetComponent<RectTransform>();
+        Update();
+    }
+
+    private void Update()
+    {
+        if (objectToFollow == null)
+            return;
+
+        if (canvas.renderMode == RenderMode.ScreenSpaceOverlay)
+        {
+            Vector3 screenPoint = mainCamera.WorldToScreenPoint(objectToFollow.position + offset);
+
+            if (transform.position != screenPoint)
+                transform.position = screenPoint;
+        }
+
+        else if (canvas.renderMode == RenderMode.ScreenSpaceCamera)
+        {
+            Vector2 viewpointPoint = mainCamera.WorldToViewportPoint(objectToFollow.position + offset);
+
+            if (rectTransform.anchorMin != viewpointPoint)
+            {
+                rectTransform.anchorMin = viewpointPoint;
+                rectTransform.anchorMax = viewpointPoint;
+            }
+        }
+    }
+
+    public void SetObjectToFollow(Transform objectToFollow)
+    {
+        this.objectToFollow = objectToFollow;
+    }
+}
---
+++ Reflected/Assets/Scripts/GUI/HoverHealthBar.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverHealthBar : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Slider slider;
+    [SerializeField] private FollowInWorldObject followInWorldObject;
+    [SerializeField] private Animator animator;
+    [SerializeField] private AnimationClip hideClip;
+
+    public Slider Slider => slider;
+    public FollowInWorldObject FollowInWorldObject => followInWorldObject;
+
+    private void Start()
+    {
+        animator.SetTrigger("Show");
+    }
+
+    private void Update()
+    {
+        // Update delayed slider
+    }
+
+    public void Remove()
+    {
+        animator.SetTrigger("Hide");
+        Destroy(gameObject, hideClip.length);
+    }
+
+}
