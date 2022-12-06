827e020d0822aa5a8b83ae45bc1318aad43bf1ff
pangquist <par.angquist@gmail.com>
2022-12-06 02:04:37 +0100

    Hide objects between player and camera

---
+++ Reflected/Assets/HideObstructions.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObstructions : MonoBehaviour
+{
+    [SerializeField] List<GameObject> currentlyInTheWay;
+    [SerializeField] List<GameObject> alreadyHidden;
+    [SerializeField] Transform player;
+    private Transform camera;
+
+    private void Awake()
+    {
+        camera = this.gameObject.transform;
+    }
+
+    void Update()
+    {
+        GetAllObjectsInTheWay();
+        ShowObjects();
+        HideObjects();
+    }
+
+    private void GetAllObjectsInTheWay()
+    {
+        currentlyInTheWay.Clear();
+
+        float cameraPlayerDistance = Vector3.Magnitude(camera.position - player.position);
+
+        Ray rayForward = new Ray(camera.position, player.position - camera.position);
+        Ray rayBackward = new Ray(player.position, camera.position - player.position);
+
+        var hitsForward = Physics.RaycastAll(rayForward, cameraPlayerDistance);
+        var hitsBackward = Physics.RaycastAll(rayBackward, cameraPlayerDistance);
+
+        foreach(var hit in hitsForward)
+        {
+            if(hit.collider.gameObject.TryGetComponent(out MeshRenderer mesh))
+            {
+                if (!currentlyInTheWay.Contains(hit.collider.gameObject))
+                {
+                    currentlyInTheWay.Add(hit.collider.gameObject);
+                }
+            }
+        }
+
+        foreach (var hit in hitsBackward)
+        {
+            if (hit.collider.gameObject.TryGetComponent(out MeshRenderer mesh))
+            {
+                if (!currentlyInTheWay.Contains(hit.collider.gameObject))
+                {
+                    currentlyInTheWay.Add(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+
+    private void HideObjects()
+    {
+        for (int i = 0; i < currentlyInTheWay.Count; i++)
+        {
+            if(currentlyInTheWay[i].TryGetComponent(out MeshRenderer mesh))
+            {
+                if (!alreadyHidden.Contains(currentlyInTheWay[i]))
+                {
+                    mesh.enabled = false;
+                    alreadyHidden.Add(currentlyInTheWay[i]);
+                }
+            }
+        }
+    }
+
+    private void ShowObjects()
+    {
+        for (int i = alreadyHidden.Count - 1; i >= 0; i--)
+        {
+            if (alreadyHidden[i].TryGetComponent(out MeshRenderer mesh))
+            {
+                if (!currentlyInTheWay.Contains(alreadyHidden[i]))
+                {
+                    mesh.enabled = true;
+                    alreadyHidden.Remove(alreadyHidden[i]);
+                }
+            }
+        }
+    }
+}
