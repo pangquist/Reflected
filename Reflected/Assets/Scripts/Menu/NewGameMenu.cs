b21598e7f7c620e73bb8163168f0e1d7db75f8e2
Kevin Bjørn Roulund Wilken <81421431+kwilken1608@users.noreply.github.com>
2022-12-20 17:59:54 +0100

    Added more loading screen tips
    that are actually useful lol

--- Reflected/Assets/Scripts/Menu/NewGameMenu.cs
+++ Reflected/Assets/Scripts/Menu/NewGameMenu.cs
@@ -8,13 +8,13 @@ using UnityEngine.UI;
 public class NewGameMenu : Menu
 {
     TextMeshProUGUI tipText;
-    string[] tipList = new string[] 
-    { 
+    string[] tipList = new string[]
+    {
         "try not to die",
         "collected coins can be used in the shop",
         "there are different types of chests, each with a specific tier of loot",
         "do NOT anger George",
-        "to kill enemies, try hitting them with your sword", 
+        "to kill enemies, try hitting them with your sword",
         "every time you kill an enemy, its soul is tortured in hell for a thousand years",
         "are we the baddies?",
         "gardening-simulator 2022",
@@ -41,7 +41,19 @@ public class NewGameMenu : Menu
         "if you play the sound of George in reverse, you will hear that it is actually Gustaf singing",
         "during development the team consumed a collected amount of coffee weighing the same as a small elephant",
         "if the chests have different tiers of loot it's just a way to trick you!",
-        "if you do not swap dimension every now and then, a flasher will start chasing you"
+        "if you do not swap dimension every now and then, a flasher will start chasing you",
+        "the sun blossom may look friendly compared to the other creatures, but that is all a ruse",
+        "beware of the poisonous gas that the bud will bombard you with",
+        "the sun blossoms spores may not hurt initially, but they will poison and slow you down over time",
+        "watch out for the hermit king, as he is both tougher and stronger than the standard shells",
+        "clearing a room of enemies will reward you with a dimension charge",
+        "ranged enemies will be scared of you when you are close to them",
+        "keep moving",
+        "do not get surrounded",
+        "if you need help, consult the tutorial dummy... dummy!",
+        "if the sun blossom starts charging its attack, you better run or finish it before it explodes",
+        "need defensive upgrades? swap to the mirror dimension",
+        "need offensive upgrades? swap to the true dimension"
     };
 
     protected override void Start()
